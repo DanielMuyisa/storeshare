09d4f6302e28231bead68eda5381cd1b518469ed
Daniel Muyisa <danielmuyisa37@gmail.com>
2022-09-06 20:36:24 +0100

    update

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 
 export default function App() {
-  return <div>
+  return <div id='root'>
     <Button variant="contained">Hello World</Button>
   </div>
 };
