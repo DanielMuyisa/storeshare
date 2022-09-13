83b15e039f5fc768324e4c1c19fed573b871dfab
Daniel Muyisa <danielmuyisa37@gmail.com>
2022-09-13 23:12:22 +0100

    update

--- src/components/modelArticle.js
+++ src/components/modelArticle.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function ModelArticle() {
     return (
-        <div>ModelArticle</div>
+        <>
+            <div>ModelArticle</div>
+            <div> Model Article</div>
+        </>
     )
 }
