8515e137a9078073ea3b90f3a2760b148f0dd1cb
Daniel Muyisa <danielmuyisa37@gmail.com>
2022-09-20 22:30:13 +0100

    update

--- src/constant/FIcon.js
+++ src/constant/FIcon.js
@@ -1,4 +1,5 @@
 import Store from '@mui/icons-material/Storefront'
 import Man from '@mui/icons-material/AccountCircleOutlined';
+// import Man from '@mui/icons-material/AccountCircleOutlined';
 
 export const FIcon = { Store, Man }
